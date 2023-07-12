04028d1497cedd22624c7154dddf09ef149cfc74
Oleksandr <bajmutov@gmail.com>
2023-07-12 17:03:36 +0300

    Update 02-video.js
    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const STORAGE_KEY = "videoplayer-current-time";
+const videoElement = document.querySelector('#vimeo-player');
+
+const player = new Player(videoElement, {
+    id: 19231868,
+    width: 1080
+});
+
+player.on('timeupdate', throttle(function(data) {
+  console.log(data)
+  const {seconds} = data;
+  localStorage.setItem(STORAGE_KEY, seconds);
+},1000));
+
+const localTime = localStorage.getItem(STORAGE_KEY);
+
+player.setCurrentTime(localTime);
 
